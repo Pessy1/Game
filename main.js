a949e368c23df69bb74e3077df9e04d2e5fb795c
Damayn <78762186+Damayn@users.noreply.github.com>
2021-02-09 01:02:03 +0200

    Idle animation

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 'use strict'
 const Game = new Phaser.Game(800, 800, Phaser.AUTO, 'game-canvas', { preload, create, update })
 
+let pl
+let music
+
 function preload() {
+    Game.load.spritesheet ('Idle_Animation','Untitled-2.png',11748/6.09,4026/1)
     Game.load.audio('music', "Naruto Theme - The Raising Fighting Spirit (320  kbps).mp3")
 }
 
 function create() {
     musicandsound ()
+    pl = Game.add.sprite(100,100,'Idle_Animation')
+    pl.scale.setTo(0.1)
+    pl.animations.add ('Idle.Animation',[0,1,2,3,4,5],6,true).play()
 }
 
 function update() {
