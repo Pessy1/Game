ac4ebf0f58527058c9edf95f0a721142e05ed051
Damayn <78762186+Damayn@users.noreply.github.com>
2021-02-09 01:44:44 +0200

    Idle animation & music & playermovment

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const Game = new Phaser.Game(800, 800, Phaser.AUTO, 'game-canvas', { preload, cr
 
 let pl
 let music
+let button
+let A,D
+let speed = 20
 
 function preload() {
     Game.load.spritesheet ('Idle_Animation','Untitled-2.png',11748/6.09,4026/1)
@@ -11,13 +14,11 @@ function preload() {
 
 function create() {
     musicandsound ()
-    pl = Game.add.sprite(100,100,'Idle_Animation')
-    pl.scale.setTo(0.1)
-    pl.animations.add ('Idle.Animation',[0,1,2,3,4,5],6,true).play()
+    playerf ()
 }
 
 function update() {
-
+    playermovment()
 }
 
 const musicandsound = function () {
@@ -25,4 +26,22 @@ const musicandsound = function () {
 
     music.play();
     
+}
+
+const playerf = function () {
+    pl = Game.add.sprite(100,100,'Idle_Animation')
+    pl.scale.setTo(0.05)
+    pl.animations.add ('Idle.Animation',[0,1,2,3,4,5],6,true).play()
+    Game.physics.enable (pl)
+    pl.body
+}
+
+const playermovment = function (){
+    pl.body.velocity.x=0
+    if (Game.input.keyboard.addKey(Phaser.Keyboard.D).isDown){
+        pl.body.velocity.x = +speed
+    }
+    if (Game.input.keyboard.addKey(Phaser.Keyboard.A).isDown){
+        pl.body.velocity.x=-speed
+    }
 }
