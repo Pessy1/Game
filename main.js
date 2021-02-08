75a49e20f9428ad0c4f16fd18ce77e514ea648ba
Damayn <78762186+Damayn@users.noreply.github.com>
2021-02-09 00:45:27 +0200

    Idle animation
    Idle анимацията

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 'use strict'
 const Game = new Phaser.Game(800, 800, Phaser.AUTO, 'game-canvas', { preload, create, update })
 
-function preload() {
+let pl
 
+function preload() {
+    Game.load.spritesheet ('player','Untitled-2.png',11748/6.09,4026/1)
 }
 
 function create() {
-    console.log('Hello there :)')
+    pl=Game.add.sprite (100,100,'player')
+    pl.animations.add ('',[0,1,2,3,4,5,6],8,true).play()
+    pl.scale.setTo(0.1)
 }
 
 function update() {
