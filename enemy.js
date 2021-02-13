9ce0b9507061ec60c6819f45b1169ee30d99427d
Damayn <78762186+Damayn@users.noreply.github.com>
2021-02-13 08:25:08 +0200

    А

--- enemy.js
+++ enemy.js
@@ -1,11 +1,16 @@
 const enemy_create = function() {
-    enemy = Game.add.sprite(Game.width-200,0,'blue')
-    enemy.scale.setTo(0.5)
+    enemy = Game.add.sprite(Game.width-200,0,'Archerer')
+    enemy.scale.setTo(-2,2)
     enemy.health = 50
+    enemy.animations.add('idle',[0,2,4,6,8,10,12,14,16,18],10,true).play()
 }
 
 const enemy_phys = function() {
     Game.physics.arcade.enable(enemy)
     enemy.body.gravity.y = 500
     enemy.body.collideWorldBounds = true
+}
+
+const archerer_movment = function () {
+
 }
--- main.js
+++ main.js
@@ -15,22 +15,24 @@ let healthbar_enemy
 let enemy
 let heal
 
+
 function preload() {
-    Game.load.spritesheet ('player','Untitled-3 (1).png',800/8,480/6)
+    Game.load.spritesheet ('player','Player_left_right.png',701/14.05,587/16.38)
+    Game.load.spritesheet ('player_left','asddsaasd2.png')
     Game.load.audio('music', "Naruto Theme - The Raising Fighting Spirit (320  kbps).mp3")
     Game.load.audio("jumpsound", "Jump.wav")
     Game.load.audio("footstep", "Footstep1.wav")
     Game.load.image ('platform','download (1).png')
-    Game.load.image('blue', 'blue.png')
+    Game.load.spritesheet ('Archerer', 'Archerer.png',966/20,112/3)
     Game.load.image('ball', 'heal.png')
     Game.load.image('healthbar', 'platform.jpg')
+    Game.load.spritesheet ('arrow','Move.png',48/2,5)
 }
 
 function create() {
     musicandsound ()
     plat=Game.add.sprite (window.innerWidth,window.innerHeight,'platform')
     plat.anchor.setTo(1,1)
-    plat.width = window.innerWidth
     playerf ()
     plAnim ()
     enemy_create()
@@ -52,6 +54,8 @@ function create() {
     healthbar_enemy.anchor.setTo(0.5, 1)
 
     Game.stage.backgroundColor = "#4488AA"
+    console.log (pl.animations)
+    
 
 }
 
@@ -71,7 +75,7 @@ function update() {
     healthbar.width = pl.health
 
     healthbar.x = pl.x+pl.width/2
-    healthbar.y = pl.y+10
+    healthbar.y = pl.y
 
     healthbar_enemy.width = enemy.health
 
@@ -91,58 +95,63 @@ const musicandsound = function () {
 
 const playerf = function () {
     
-    pl=Game.add.sprite (100,100,'player')
+    pl=Game.add.sprite (100,window.innerHeight,'player')
     pl.scale.setTo(3)
     pl.health = 80
     Game.physics.enable (pl)
     pl.body.collideWorldBounds = true
     pl.body.gravity.y = 400
+    pl.body.setSize (38,31)
 }
-
 const plAnim = function() {
-    pl.animations.add('Idle',[0,1,2,3,4,5],8,true)
-    pl.animations.add('Idle_left', [31,30,29,28,27,26],8,true)
-    pl.animations.add('Running',[8,9,10,11,12,13,14],10)
-    pl.animations.add('Running_left',[39,38,37,36,35,34,32],10)
-    pl.animations.add('Jump',[19,20,21],10)
-    pl.animations.add('Jump_left',[44,43,42],10)
+    pl.animations.add('Idle',[7,8,9,10],4.8,true)
+    pl.animations.add('Idle_left', [6,5,4,3],4.8,true)
+    pl.animations.add('Running',[22,23,24,25,26,27],6.5)
+    pl.animations.add('Running_left',[19,18,17,16,15,14],6.5)
+    pl.animations.add('Jump',[35,36,37,38,39,40,41,49,50],10,false)
+    pl.animations.add('Jump_left',[34,33,32,31,30,29,28,48,47],10)
+    pl.animations.add('Top',[51],10)
+    pl.animations.add('Top_left',[46],10)
 }
-
 const playermovment = function (){
     pl.body.velocity.x=0
 
     if (Game.input.keyboard.addKey(Phaser.Keyboard.A).isDown){
         pl.body.velocity.x=-speed
+        if (pl.body.onFloor()===true){
         pl.animations.play('Running_left')
+        }
         direction = 'left'
     }else if (Game.input.keyboard.addKey(Phaser.Keyboard.D).isDown){
         pl.body.velocity.x = +speed
+        if (pl.body.onFloor()===true){
         pl.animations.play('Running')
+    }
         direction = 'right'
-    }else if(direction === 'right'){
+    }else if(direction === 'right'&&pl.body.onFloor()===true){
         (pl.animations.play('Idle'))
-    }else if (direction === 'left'){
+    }else if (direction === 'left'&&pl.body.onFloor()===true){
         pl.animations.play('Idle_left')
     }
     if (Game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).isDown && pl.body.onFloor() && Game.time.now > jumpTimer)
     {
-        pl.body.velocity.y = -450;
-        jumpTimer = Game.time.now + 750;
-    }
-    if (pl.body.onFloor() === false){
+        pl.body.velocity.y = -450
+        jumpTimer = Game.time.now + 750
+        
         if(direction === 'right'){
-            (pl.animations.play('Jump'))
+            pl.animations.play('Jump',10,false)
         }else if (direction === 'left'){
-            pl.animations.play('Jump_left')
+           pl.animations.play('Jump_left')} 
+    }
+    if (pl.body.onFloor() === false&&pl.body.velocity.y<-450){
+         if(direction === 'right'){
+            pl.animations.play('Top',10,false)
+        }else if (direction === 'left'){
+           pl.animations.play('Top_left')
         }
     }
-
-    // Jump!
     
-
-    Game.physics.arcade.collide (pl,plat)
-    }
-
+}
 const death = function() {
     if (pl.health < 0){
         healthbar.destroy()
@@ -170,4 +179,3 @@ const healing = function() {
     }    
 }
         
-    
