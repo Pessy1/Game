9b0b5f530378b2295ff8f62e40f61fdd78b31cc1
Stan0000 <zdrkopras123@gmail.com>
2021-02-27 11:17:56 +0200

    mjnk
    nhbg

--- player.js
+++ player.js
@@ -1,6 +1,6 @@
 const playerf = function () {
     
-    pl=Game.add.sprite (3800, 3300,'player')
+    pl=Game.add.sprite (0, 3300,'player')
     pl.anchor.setTo(0.5, 0)
     pl.scale.setTo(3)
     pl.health = 80
